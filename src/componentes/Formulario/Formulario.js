13c3eb9312f6c149de2432127a9e2aa2c99d459c
Yasmin <yasminmath23@gmail.com>
2025-08-27 15:55:14 -0300

    Alterando comportamento padrão

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -17,14 +17,16 @@ import Botao from '../Botao/Botao';
 
 const Formulario = () => {
 
-    const aoSalvar = () => console.log("Documento salvo")
+    const aoSalvar = (e) => {
+        e.preventDefault()
+        console.log("Documento salvo")}
     return (
         <section className='section-login'>
             <form className='form-login' onSubmit={aoSalvar} >
                 <h2>Preencha os dados para criar o card do jogador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-                <ListaSuspensa label="Jogos" itens={jogos} />
+                <CampoTexto label="Nome" obrigatorio={true} placeholder="Digite seu nome" />
+                <CampoTexto label="Imagem"  placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Jogos" obrigatorio={true} itens={jogos} />
                 <Botao>
                     Criar Botão
                 </Botao>
--- src/componentes/ListaSuspensa/ListaSuspensa.js
+++ src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -4,7 +4,7 @@ const ListaSuspensa = (props) => {
     return (
         <div className='ListaSuspensa'>
             <label>{props.label}</label>
-            <select>
+            <select required={props.obrigatorio}>
                 <option></option>
                 {props.itens.map(itens => <option key={itens}>{itens}</option>)}
             </select>
