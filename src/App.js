ac7fa2951f949ad4c9bfc06fc4ac3612a1451ef1
Yasmin <yasminmath23@gmail.com>
2025-08-29 17:15:55 -0300

    Criando o componente Jogo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
+import Jogo from './componentes/Jogo/Jogo'
 
-const [colaboradores, setColaborades] = useState([])
-
-const aoNovoColaboradorAdcionado = (colaborador) =>
-{
-  setColaborades([...colaboradores, colaborador])
-}
 function App() {
+  const [colaboradores, setColaborades] = useState([])
+  const aoNovoColaboradorAdcionado = (colaborador) => {
+    setColaborades([...colaboradores, colaborador])
+  }
   return (
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdcionado(colaborador)} />
+      <Jogo nome="Xadrez" />
+      <Jogo nome="Damas"/>
     </div>
   );
 }
---
+++ src/componentes/Jogo/Jogo.js
@@ -0,0 +1,14 @@
+import "./Jogo.css"
+
+const Jogo = (props) =>
+{
+    return (
+
+        <section className="sectionJogo">
+            <h1>{props.nome}</h1>
+
+        </section>
+    )
+}
+
+export default Jogo
